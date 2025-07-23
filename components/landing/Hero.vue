50dca1aa996866f7e63cef057f3660e28007d2b7
zice <zhaobing0503@gmail.com>
2025-07-23 11:03:23 +0800

    feat(landing): add GitHub button link to homepage Add a GitHub button to the homepage hero section for easy access to the project repository

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -40,6 +40,13 @@
           </button>
           </NuxtLink>
           
+          <a href="https://github.com/zzice/nuxt-plus" target="_blank" rel="noopener noreferrer">
+            <button
+            class="inline-flex h-11 items-center justify-center rounded-md bg-secondary px-8 py-2 text-sm font-medium text-secondary-foreground shadow hover:bg-secondary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 gap-4">
+            {{ $t('landing.hero.github') }}
+            <Icon name="lucide:github" class="w-4 h-4" />
+          </button>
+          </a>
         </div>
       </div>
     </div>
